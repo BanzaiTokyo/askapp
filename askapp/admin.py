497466ff45bbec0251ce74ce1e966fa5365fe6eb
Anton Alekseev <toxa23@gmail.com>
2017-01-06 09:36:46 +0300

    admin: display profile inside user

--- askapp/admin.py
+++ askapp/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from askapp.models import Profile, Thread, Tag
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email', 'country')
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
 
 
 class ThreadAdmin(admin.ModelAdmin):
@@ -14,6 +22,7 @@ class TagAdmin(admin.ModelAdmin):
     exclude = ('slug', )
 
 
-admin.site.register(Profile, ProfileAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Thread, ThreadAdmin)
 admin.site.register(Tag, TagAdmin)
