aff9eadc354187f610144c9e117d592860306d78
Anton Alekseev <toxa23@gmail.com>
2017-02-02 12:37:28 +0300

    fix unicode problem for Py2

--- askapp/models.py
+++ askapp/models.py
@@ -207,7 +207,7 @@ class Thread(models.Model):
         self._old = model_to_dict(self, fields=['id', 'hidden', 'closed', 'sticky', 'sponsored', 'deleted', 'text', 'title'])
 
     def __str__(self):
-        return str(self.title)
+        return self.title.encode('utf-8')
 
     def save(self, *args, **kwargs):
         self.prepare_images()
