119f622a59ff4fe264c4718212a81f2f52b1e488
Anton Alekseev <toxa23@gmail.com>
2017-03-29 21:41:19 +0300

    a bit of explanations for sitemap #59

--- askapp/sitemaps.py
+++ askapp/sitemaps.py
@@ -5,10 +5,11 @@ from django.template.defaultfilters import slugify
 
 class ThreadSitemap(Sitemap):
     """
-    What does this class do?
+    This class generates sitemap subset for thread URLs
+    https://docs.djangoproject.com/en/1.8/ref/contrib/sitemaps/#sitemap-classes
     """
 
-    #what is changefreq?
+    # Check attributes and methods destination (except items) at https://www.sitemaps.org/protocol.html#urlsetdef
     changefreq = "never"
 
     priority = 0.5
@@ -25,7 +26,7 @@ class ThreadSitemap(Sitemap):
 
 class ProfileSitemap(Sitemap):
     """
-    Some documentation would be helpful
+    This class generates sitemap subset for profile URLs
     """
     changefreq = "never"
     priority = 0.5
@@ -39,7 +40,10 @@ class ProfileSitemap(Sitemap):
     def location(self, obj):
         return '/profile/{}/{}/'.format(obj.id, slugify(obj))
 
-# what does this dictionary do?
+# this is a dictionary of the site sitemaps. Since our sitemap includes dynamic content, it can be large enough and
+# overboard the standard limit of 50,000 entries (https://www.sitemaps.org/protocol.html#index). Hence we split the
+# sitemap into two lists: threads and profiles and create an index file.
+# https://docs.djangoproject.com/en/1.8/ref/contrib/sitemaps/#creating-a-sitemap-index
 sitemap_dict = {
     'threads': ThreadSitemap,
     'profiles': ProfileSitemap,
