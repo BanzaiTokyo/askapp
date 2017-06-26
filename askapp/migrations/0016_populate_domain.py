a892947c40d989d93b088a771b37ec24c2ee95b9
Anton Alekseev <toxa23@gmail.com>
2017-06-26 11:55:03 +0300

    bugfix in thread.domain data migration

--- askapp/migrations/0016_populate_domain.py
+++ askapp/migrations/0016_populate_domain.py
@@ -3,11 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+try:
+    from urllib.parse import urlparse
+except:
+    from urlparse import urlparse
 
 def populate_domain(apps, schema_editor):
     threads = apps.get_model('askapp', 'Thread')
     for thread in threads.objects.all():
-        thread.save()
+        if thread.link:
+            hostname = urlparse(thread.link)
+            thread.domain = hostname.netloc
+            thread.save()
 
 
 class Migration(migrations.Migration):
