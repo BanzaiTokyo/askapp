a261e2987a738ba8e2e422f23796c00eca0882e4
Anton Alekseev <toxa23@gmail.com>
2017-07-07 10:04:05 +0200

    commented models.py; make most properties cached

--- askapp/models.py
+++ askapp/models.py
@@ -28,7 +28,11 @@ from markdownx.models import MarkdownxField
 from askapp import settings
 from siteprefs.models import Preference
 
+
 class OverwriteStorage(FileSystemStorage):
+    """
+    When saving an image, this storage class deletes existing file, thus implementing the overwriting feature
+    """
     def get_available_name(self, name):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
@@ -36,12 +40,18 @@ class OverwriteStorage(FileSystemStorage):
 
 
 def avatar_name_path(instance, filename):
+    """
+    convert arbitrary file name to the string consisting of username and user ID
+    """
     extension = filename[filename.rfind('.'):]
     new_path = 'user_profile/%s%s%s' % (instance.user.username, instance.user.pk, extension)
     return new_path
 
 
 class AskappImageFieldFile(ImageFieldFile):
+    """
+    Return default avatar if there is no image
+    """
     @property
     def url(self):
         try:
@@ -58,21 +68,28 @@ class AskappImageField(models.ImageField):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """
+    Additional user properties. Attached to the Django user object by 1-to-1 relation
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # 1-to-1 relation to the builtin Django User object
     avatar = AskappImageField(storage=OverwriteStorage(), upload_to=avatar_name_path, blank=True)
-    country = CountryField(blank=True)
-    city = models.CharField(max_length=50, blank=True)
-    about = models.TextField(max_length=500, blank=True)
+    country = CountryField(blank=True)  # country of residence. 3rd party component, saved as ISO code, displayed as full name. Input control is a dropdown list, supporting localisation
+    city = models.CharField(max_length=50, blank=True)  # city of residence
+    about = models.TextField(max_length=500, blank=True)  # "about me", biography text field
 
     def __init__(self, *args, **kwargs):
         super(Profile, self).__init__(*args, **kwargs)
+        # __original_avatar is used to detect avatar change, to run avatar resize procedure only when needed
         self.__original_avatar = self.avatar
 
     def __unicode__(self):
         return self.user.username
 
-    @property
+    @cached_property
     def email(self):
+        """
+        a helper that allows get email even from the Profile object
+        """
         return self.user.email
 
     def resize_avatar(self):
@@ -107,15 +124,11 @@ class Profile(models.Model):
             self.resize_avatar()
         super(Profile, self).save(*args, **kwargs)
 
-    def get_avatar(self):
-        if self.avatar:
-            avatar_url = self.avatar.url
-        else:
-            avatar_url = settings.STATIC_URL + 'images/avatar.png'
-        return avatar_url
-
     @cached_property
     def favorite_threads(self):
+        """
+        get list of the threads that user has "upvoted"
+        """
         favorites = ThreadLike.objects.filter(user=self.user, points__gt=0).order_by('-created')
         threads = [f.thread for f in favorites]
         return threads
@@ -123,24 +136,33 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+     Signal handler running after Django.user object is saved. This handler creates a Profile object for new user.
+     And for disabled user it marks as "deleted" all user's threads and posts
+    """
     if not instance.is_active:
         if kwargs['update_fields'] and 'is_active' in kwargs['update_fields']:
+            # Perform posts/threads deletion only when is_active field was explicitly mentioned for update
             # delete user threads and posts
             Post.objects.filter(user_id=instance.id, deleted=False).update(deleted=True)
             Thread.objects.filter(user_id=instance.id, deleted=False).update(deleted=True)
     elif created:
         return Profile.objects.create(user=instance)
     else:
+        # even though "created" is False (this is a regular update), ensure that the Profile object exists
         try:
             profile = instance.profile
         except:
-            profile = create_user_profile(None, instance, True)
+            profile = Profile.objects.create(user=instance)
             instance.profile = profile
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=60, null=False)
-    slug = models.SlugField(max_length=60, null=False)
+    """
+    Thread tags
+    """
+    name = models.CharField(max_length=60, null=False)  # full tag name
+    slug = models.SlugField(max_length=60, null=False)  # slugified tag name to use in URLs
 
     def __unicode__(self):
         return self.name
@@ -154,6 +176,9 @@ class Tag(models.Model):
 
 
 class Thread(models.Model):
+    """
+    Thread is the main content object of the application
+    """
     # codes for types of posts
     QUESTION = "QQ"
     DISCUSSION = "DD"
@@ -171,11 +196,11 @@ class Thread(models.Model):
     tags = models.ManyToManyField(Tag, blank=True, verbose_name=_('tags'))
 
     # these fields are taken into account only if the post is thread starter
-    hidden = models.BooleanField(default=False) # the thread is visible only to the staff and the author
-    closed = models.BooleanField(default=False) # noone can post comments / answers in this thread
-    sticky = models.DateField(null=True, blank=True) # this thread will be sticky until the given date
-    featured = models.BooleanField(default=False) # hopefully one day there will be sponsored threads...
-    deleted = models.BooleanField(default=False) # the thread is marked as deleted, usually on user blocking
+    hidden = models.BooleanField(default=False)  # the thread is visible only to the staff and the author
+    closed = models.BooleanField(default=False)  # no one can post comments / answers in this thread
+    sticky = models.DateField(null=True, blank=True)  # this thread will be sticky until the given date
+    featured = models.BooleanField(default=False)  # hopefully one day there will be sponsored threads...
+    deleted = models.BooleanField(default=False)  # the thread is marked as deleted, usually on user blocking
 
     # reference to the user who created the post
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
@@ -227,7 +252,7 @@ class Thread(models.Model):
         self.prepare_images()
         self.update_link()
         super(Thread, self).save()
-        AuditThread.audit(self)
+        AuditThread.audit(self)  # log all changes applied to the thread
 
     def prepare_images(self):
         if not self.id:
@@ -235,12 +260,16 @@ class Thread(models.Model):
         try:
             this = Thread.objects.get(id=self.id)
             if this.image != self.image:
+                #delete old image explicitly, as new image will have different name
                 this.image.delete(False)
                 this.thumbnail.delete(False)
         except Exception as ex:
             pass
 
     def update_link(self):
+        """
+        extract domain name for threads of type "link"
+        """
         if self.thread_type != 'LL':
             self.link = None
             self.domain = None
@@ -248,9 +277,9 @@ class Thread(models.Model):
             hostname = urlparse(self.link)
             self.domain = hostname.netloc
 
-    @property
+    @cached_property
     def comments(self):
-        if self.thread_type == self.QUESTION:
+        if self.thread_type == self.QUESTION:  # filter out comments marked as answers, they'll come in another property
             params = {
                 'is_answer': False,
                 'deleted': False,
@@ -261,34 +290,37 @@ class Thread(models.Model):
             result = self.post_set.all()
         return result
 
-    @property
+    @cached_property
     def answers(self):
         x = self.post_set.filter(is_answer=True, deleted=False)
         return x
 
-    @property
+    @cached_property
     def num_comments(self):
         return self.post_set.filter(deleted=False).count()
 
-    @property
+    @cached_property
     def points(self):
         result = self.threadlike_set.all().aggregate(sum=models.Sum('points'))['sum']
         return result or 0
 
-    @property
+    @cached_property
     def author(self):
         return self.user.username
 
-    @property
+    @cached_property
     def answered(self):
+        """
+        check whether the thread of type "question" has an answer, to prevent marking another comment as the answer
+        """
         return self.answers.filter(accepted__isnull=False).count() > 0
 
 
 class Post(MPTTModel):
-    '''
+    """
     Post is a part of the discussion on the levels below Thread
     It can be comments, answers organized in several levels
-    '''
+    """
 
     # defines the parent post. If the value is null, the post is a thread starter
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
@@ -317,62 +349,34 @@ class Post(MPTTModel):
     def __str__(self):
         return str(self.id)
 
-    @property
+    @cached_property
     def points(self):
         result = self.postlike_set.all().aggregate(sum=models.Sum('points'))['sum']
         return result or 0
 
-    @property
+    @cached_property
     def author(self):
         return self.user.username
 
-    @property
+    @cached_property
     def comments(self):
         return self.get_children().filter(deleted=False)
 
     def accept(self):
+        """
+        accept this comment as the answer for the thread type "question"
+        """
         self.accepted = datetime.utcnow()
         self.save()
 
 
-class Action(models.Model):
-    '''
-    Actions are taken by users and can describe:
-    - update post (saving the old text and title)
-    - close post
-    - setting post sticky, etc
-    '''
-
-    TYPES_OF_ACTION = (
-        ("update", 'Update'),
-        ("close", 'Close'),
-        ("sticky", 'Sticky'),
-    )
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-
-    post = models.ForeignKey(Post, null=True)
-
-    # atomatically added timestamp field when the record is created
-    taken_on = models.DateTimeField(auto_now_add=True)
-
-    # action
-    action_name = models.TextField(null=False, choices=TYPES_OF_ACTION, default="update")
-
-    # old post body
-    old_text = models.TextField(null=True)
-
-    # old post title
-    old_title = models.TextField(null=True)
-
-
 class ThreadLike(models.Model):
-    '''
+    """
     Users can give up- and down-votes to threads. Upvote = +1, downvote = -1.
     Regular users cannot "like" their own thread.
     Regular users cannot "like" others' threads more than once.
     Threads with positive likes are diplayed in user's "favorites" page.
-    '''
+    """
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
@@ -397,9 +401,11 @@ class ThreadLike(models.Model):
 
 
 class PostLike(models.Model):
-    '''
-
-    '''
+    """
+    Users can give up- and down-votes to comments. Upvote = +1, downvote = -1.
+    Regular users cannot "like" their own comments.
+    Regular users cannot "like" others' comments more than once.
+    """
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -424,6 +430,14 @@ class PostLike(models.Model):
 
 
 class AuditThread(models.Model):
+    """
+    Audit user actions
+    Actions are taken by users and can describe:
+    - update post (saving the old text and title)
+    - close post
+    - setting post sticky, etc
+    """
+
     TYPES_OF_ACTION = (
         ("update", 'Update'),
         ("close", 'Close'),
@@ -436,7 +450,7 @@ class AuditThread(models.Model):
     thread = models.ForeignKey(Thread)
     action = models.TextField(null=False, choices=TYPES_OF_ACTION, default="update")
     created = models.DateTimeField(auto_now_add=True)
-    content = models.TextField(null=True)
+    content = models.TextField(null=True)  # old title or text of the edited post
 
     @classmethod
     def audit(cls, instance):
@@ -468,7 +482,7 @@ class AuditThread(models.Model):
 def clear_language_cache(sender, instance, **kwargs):
     """
     This function clears lru cache of django.utils.translation.trans_real.get_supported_language_variant
-    Otherwise this function ignores settings.LANGUAGE_CODE change
+    Otherwise Django ignores settings.LANGUAGE_CODE change
     """
     if instance.name == 'language_code':
         from django.utils.translation.trans_real import get_supported_language_variant
