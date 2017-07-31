d7a9cf0ca4272bc222da3a50ea41809820f65283
Anton Alekseev <toxa23@gmail.com>
2017-07-31 20:09:22 +0300

    google analytics ID

--- askapp/context_processors.py
+++ askapp/context_processors.py
@@ -1,4 +1,4 @@
-from .settings import SITE_NAME, ABOUT_TEXT
+from .settings import SITE_NAME, ABOUT_TEXT, GOOGLE_ANALYTICS_ID
 
 def site_processor(request):
-    return { 'site_name': SITE_NAME, 'about_text': ABOUT_TEXT }
+    return { 'site_name': SITE_NAME, 'about_text': ABOUT_TEXT, 'google_analytics_id': str(GOOGLE_ANALYTICS_ID) }
