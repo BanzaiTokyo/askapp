623290780cd2e806e319098188c872d3f958c8b6
Anton Alekseev <toxa23@gmail.com>
2020-09-25 10:21:10 +0300

    remove customized siteprefs

--- siteprefs/__init__.py
+++
@@ -1,4 +0,0 @@
-VERSION = (0, 7, 0)
-
-
-default_app_config = 'siteprefs.config.SiteprefsConfig'
--- siteprefs/exceptions.py
+++
@@ -1,3 +0,0 @@
-
-class SitePrefsException(Exception):
-    """Base exception class for SitePrefs."""
