d5928d7a77a3ee385be2508dff71c26cff421f9d
Anton Alekseev <toxa23@gmail.com>
2021-01-15 09:49:36 +0300

    fix migrations for auth token

--- askapp/migrations/0021_auth_token.py
+++ askapp/migrations/0021_auth_token.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('askapp', '0020_auto_20201101_1818'),
     ]
 
     operations = [
